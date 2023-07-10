18b28e3ea90b14006f4ed42e77dd350e70cefb75
김연구 <yeongoo.kim@qualson.com>
2023-07-10 17:48:10 +0900

    fix: title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MGK manager',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
